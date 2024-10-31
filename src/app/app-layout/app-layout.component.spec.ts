a8a90a036ab50470ef89f77a54d1241090edc0c7
vitalitygo5 <vitalitygo5@gmail.com>
2024-10-31 21:25:26 +0000

    v1.03 commit

--- src/app/app-layout/app-layout.component.spec.ts
+++ src/app/app-layout/app-layout.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AppLayoutComponent } from './app-layout.component';
+import { AppLayoutComponent } from './AppLayoutComponent';
 
 describe('AppLayoutComponent', () => {
   let component: AppLayoutComponent;
--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,9 +1,13 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3852150750.
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:3613695930.
+
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from 'src/app/core/components/navbar/navbar.component';
-import { BottomNavComponent } from 'src/app/core/components/bottom-nav/bottom-nav.component';
+import { NavbarComponent } from '../layout/navbar/navbar.component';
+import { BottomNavComponent } from '../layout/bottom-nav/bottom-nav.component';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-layout',
@@ -17,4 +21,5 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app-layout.component.html',
   styleUrls: ['./app-layout.component.css']
 })
-export class AppLayoutComponent {}
+export class AppLayoutComponent {
+}
